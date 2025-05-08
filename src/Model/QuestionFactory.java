97dc1b32be813da49ca202817675c7cb3cd1fa84
Lily Hoopes <lilyhoopes@gmail.com>
2025-05-08 12:03:06 -0700

    added a couple notes for factory

--- src/Model/QuestionFactory.java
+++ src/Model/QuestionFactory.java
@@ -1,4 +1,12 @@
 package Model;
 
+//defines interface for creating an object, but lets subclasses decide which class to instantiate
+/*
+question facotry that knows how to mak questions
+just update things from in factory to get questions, these should be put into an array once they are all made
+
+
+ */
+
 public class QuestionFactory {
 }
