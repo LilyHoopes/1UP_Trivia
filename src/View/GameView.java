d43fd1efd18a4c86ffc085697042a09ee0691ffe
Lily Hoopes <lilyhoopes@gmail.com>
2025-05-13 15:29:36 -0700

    created menu class and added lots of jFrame components

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,5 +1,7 @@
 package View;
 
+import Model.TriviaQuestion;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
@@ -8,15 +10,29 @@ import javax.swing.*;
 
 //should this be extends or inherits?
 public class GameView extends JFrame implements PropertyChangeListener {
-    private JLabel myKeyInstruction, mySelectA, mySelectB, mySelectC, mySelectD;
-    private JButton myMoveUp, myMoveDown, myMoveLeft, myMoveRight, mySubmitAnswer;
-    private JMenuBar myGameMenu;
-    private JMenuItem myStart, mySave, myLoad, myReset;
-    private JMenuBar myHelp;
-    private JMenuItem myAbout, myInstructions;
+
+    //JLabels for room panel
+    private JLabel myUpLabel, myDownLabel, myLeftLabel, myRightLabel, myRoomPlayer;
+    //JButtons for room panel
+    private JButton myUpButton, myDownButton, myLeftButton, myRightButton;
+
+    //JLabels for the questions panel
+    private JLabel myQuestionLabel, myOptionA, myOptionB, myOptionC, myOptionD;
+    private JButton myA_Button, myB_Button, myC_Button, myD_Button, mySubmitButton;
+
+    //JLabels for the maze panel
+    private JLabel myStartImage, myEndImage, myMazePlayer;
+    private JLabel myA_Room, myB_Room, myC_Room, myD_Room, myE_Room, myF_Room, myG_Room,  myH_Room,
+            myI_Room, myJ_Room, myK_Room, myL_Room, myM_Room, myN_Room, myO_Room, myP_Room;
+
+    //add for loop to make a bunch of arrow aka door images to add to panel
+
+    private JButton myMoveUp, myMoveDown, myMoveLeft, myMoveRight, mySubmitAnswer
 
     public GameView() {
         // constructor initializes components
+        JPanel mainPanel = new JPanel();
+        //mainPanel.set
     }
 
     private void createMenuBar() {
@@ -51,6 +67,25 @@ public class GameView extends JFrame implements PropertyChangeListener {
 
     public void actionPerformed(ActionEvent e) {
         // handle UI actions
+    }
+
+    public static void main(String[] args) {
+        //EventQueue.invokeLater(new Runnable() {})
+        final GameView mainPanel = new GameView();
+        final Dimension frameSize = new Dimension(500, 500);
+
+        //line to add property chagne liseneters
+
+        final JFrame window = new JFrame("1UP Trivia!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(mainPanel);
+
+        //wondow.setmenu bar to add menu bar bar, calls method
+
+        window.setSize(frameSize);
+        window.pack();
+        window.setVisible(true);
+
     }
 
     @Override
