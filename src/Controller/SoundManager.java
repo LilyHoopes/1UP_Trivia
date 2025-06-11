21ef4b67aca758b96fc6f258869ac2aaefd8dcec
Lily Hoopes <lilyhoopes@gmail.com>
2025-06-11 10:30:26 -0700

    fixed the volume for sounds no more blasted ears :P

--- src/Controller/SoundManager.java
+++ src/Controller/SoundManager.java
@@ -10,13 +10,7 @@ import java.io.IOException;
  *
  * It uses a singleton pattern to make sure there's only one instance
  * managing sounds throughout the game.
- *
- * @author Komalpreet Dhaliwal
- * @author Lily Hoopes
- * @author Christiannel Maningat
- * @version 6/7/2025
  */
-
 public class SoundManager {
 
     /** The single instance of SoundManager. */
@@ -40,45 +34,46 @@ public class SoundManager {
 
     /** Plays the win sound. */
     public void playWinSound() {
-        playSoundAbsolute("sounds/Win.wav");
+        playSoundAbsolute("sounds/Win.wav", 0.8f);  // full volume
     }
 
     /** Plays the lose sound. */
     public void playLoseSound() {
-        playSoundAbsolute("sounds/Lose.wav");
+        playSoundAbsolute("sounds/Lose.wav", 0.8f);
     }
 
     /** Plays the incorrect answer sound. */
     public void playIncorrectSound() {
-        playSoundAbsolute("sounds/Incorrect.wav");
+        playSoundAbsolute("sounds/Incorrect.wav", 0.7f);
     }
 
     /** Plays the correct answer sound. */
     public void playCorrectSound() {
-        playSoundAbsolute("sounds/Correct.wav");
+        playSoundAbsolute("sounds/Correct.wav", 0.7f);
     }
 
     /** Plays the start game sound. */
     public void playStartSound() {
-        playSoundAbsolute("sounds/StartGame.wav");
+        playSoundAbsolute("sounds/StartGame.wav", 0.6f);
     }
 
     /** Plays the exit game sound. */
     public void playExitSound() {
-        playSoundAbsolute("sounds/Exit.wav");
+        playSoundAbsolute("sounds/Exit.wav", 0.8f);
     }
 
     /** Plays the invalid move sound. */
     public void playNoMoveSound() {
-        playSoundAbsolute("sounds/NoMove.wav");
+        playSoundAbsolute("sounds/NoMove.wav", 0.8f);
     }
 
     /**
-     * Plays a sound from the given file path.
+     * Plays a sound from the given file path at the given volume.
      *
      * @param filePath the path to the .wav file.
+     * @param volume a float from 0.0 (mute) to 1.0 (full volume).
      */
-    private void playSoundAbsolute(String filePath) {
+    private void playSoundAbsolute(String filePath, float volume) {
         try {
             File soundFile = new File(filePath);
             if (!soundFile.exists()) {
@@ -89,6 +84,17 @@ public class SoundManager {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
+
+            // Apply volume control
+            if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+                FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                float range = gainControl.getMaximum() - gainControl.getMinimum();
+                float gain = (range * volume) + gainControl.getMinimum();
+                gainControl.setValue(gain);
+            } else {
+                System.err.println("Volume control not supported for: " + filePath);
+            }
+
             clip.start();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
